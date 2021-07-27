4316f7897b6f8edfc406a11e861c4183d54fa76f
Sherron <sherron@Sherrons-MacBook-Air.local>
2021-07-27 17:49:50 -0400

    assigned variable for conditionals and passing second test with matching type

--- orderBook.js
+++ orderBook.js
@@ -5,5 +5,17 @@ function reconcileOrder(existingBook, incomingOrder) {
 
     return existingBook
   }
+  // 2 - if type == add to order book//
+  for (let i = 0; i < existingBook.length; i++) {
+    let matchingOrderType = (incomingOrder.type === existingBook[i].type)
+    let matchingPrice = (incomingOrder.price === existingBook[i].price)
+    let matchingQuantity = (incomingOrder.quantity === existingBook[i].quantity)
+
+    if (matchingOrderType) {
+      existingBook.push(incomingOrder)
+
+      return existingBook
+    }
+  }
 }
 module.exports = reconcileOrder
